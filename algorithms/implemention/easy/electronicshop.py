676741776789c42f2c2438988e24d8f692a96c6a
saurabh kumar giri <skgvns8789@gmail.com>
2018-06-24 18:11:38 +0530

    Add files via upload

---
+++ algorithms/implemention/easy/electronicshop.py
@@ -0,0 +1,21 @@
+ops = input().split()
+s = int(ops[0])
+mm = int(ops[1])
+nn = int(ops[2])
+m = list(map(int , input().rstrip().split()))
+n = list(map(int , input().rstrip().split()))
+
+max = -1
+for i in m:
+    for j in n:
+        a = i+j
+        # print("max is ",a)
+        if a == s:
+            print(a)
+            exit()
+        elif a < s:
+            if a > max:
+                max = a
+        else:
+            pass
+print(max)
